8722fb015e70ac69f7cb07b8b0d3a93ddd716583
oigresAoContrario <fernandes.santos.sergio@escola.pr.gov.br>
2025-10-01 15:11:43 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,11 @@
+criacartao(
+'Programação',
+'O que é JavaScript',
+'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+'Fisica',
+'Qual é a primeira Lei de Newton?',
+'Lei da Inércia'
+)
